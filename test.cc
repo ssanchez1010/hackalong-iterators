9a9a37964ae57cf65030c3abc9a5c1e0707b93bf
Sophia Sanchez <ssanchez1010@csu.fullerton.edu>
2026-03-26 18:47:53 +0000

    done

--- test.cc
+++ test.cc
@@ -1,1 +1,42 @@
 
+#include <iostream>
+#include <string>
+#include <map>
+
+int main() {
+    std::map<std::string, double> prices = {
+        {"banana", 0.99},
+        {"apple", 1.49},
+        {"pear", 1.29}
+    };
+
+    std::string fruit;
+    double price;
+
+    while (true) {
+        std::cout << "Enter a fruit name (or 'done' to quit): ";
+        std::cin >> fruit;
+        if (fruit == "done") {
+            break;
+        }
+
+        std::cout << "Enter price for " << fruit << ": ";
+        std::cin >> price;
+        prices[fruit] = price;
+    }
+
+    std::cout << "\nFruit prices:\n";
+    for (const auto& [name, p] : prices)
+        std::cout << " " << name << ": $" << p << "\n";
+    
+    std::cout << "Raising prices by 10%" << "\n";
+    double percent = .1;
+    for (std::map<std::string,double>::iterator it = prices.begin(); it != prices.end(); ++it) {
+        it ->second *= (1.0 + percent);
+    }
+
+    std::cout << "\nNew Fruit prices:\n";
+    for (const auto& [name, p] : prices)
+        std::cout << " " << name << ": $" << p << "\n";
+    return 0;
+}
